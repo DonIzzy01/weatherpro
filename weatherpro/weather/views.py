56b0698bbf2b9dc9eef7bd5f65675f602c83d62d
obi israel <isrealdegreat@gmail.com>
2024-12-31 22:12:01 +0100

    1st changes

--- weatherpro/weather/views.py
+++ weatherpro/weather/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 def index(request):
-    city = None  # Initialize city variable
+    cities = City.objects.all()  # Retrieve all cities from the database
     weather = {}
 
     if request.method == 'POST':
-        city = request.POST.get('city')  # Get the city name from the form
-        api_key = 'YOUR_API_KEY'  # Replace with your OpenWeatherMap API key
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={api_key}'
+        city_name = request.POST.get('city')  # Get the city name from the form
+        api_key = '49f21af64f749234c18f1726659bf7a1'  # Your OpenWeatherMap API key as a string
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_key}'
 
         response = requests.get(url)
         if response.status_code == 200:
             city_weather = response.json()
             weather = {
-                'city': city,
+                'city': city_name,
                 'temperature': city_weather['main']['temp'],
                 'description': city_weather['weather'][0]['description'],
                 'icon': city_weather['weather'][0]['icon']
@@ -22,4 +23,4 @@ def index(request):
         else:
             weather = {'error': 'City not found'}
 
-    return render(request, 'weather/index.html', {'weather': weather})
+    return render(request, 'weather/index.html', {'weather': weather, 'cities': cities})
